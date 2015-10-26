38303ac7373df625d241dd9729b946bc0f0bb2cb
Leonardo JG Silva <leosilvadev@gmail.com>
2015-10-25 22:44:29 -0200

    changing from jetty to use immutant

--- project.clj
+++ project.clj
@@ -8,8 +8,8 @@
 
                  ;; Remove this line and uncomment one of the next lines to
                  ;; use Immutant or Tomcat instead of Jetty:
-                 [io.pedestal/pedestal.jetty "0.4.0"]
-                 ;; [io.pedestal/pedestal.immutant "0.4.0"]
+                 ;; [io.pedestal/pedestal.jetty "0.4.0"]
+                 [io.pedestal/pedestal.immutant "0.4.0"]
                  ;; [io.pedestal/pedestal.tomcat "0.4.0"]
 
                  [ch.qos.logback/logback-classic "1.1.2" :exclusions [org.slf4j/slf4j-api]]
